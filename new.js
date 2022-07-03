09dc5f2ed73815e709a64deb32a86a876d3b7e29
shenyileirob <917514182@qq.com>
2022-07-04 02:36:54 +0800

    Update new.js

--- new.js
+++ new.js
@@ -1,35 +1,34 @@
 var db;
-var objectStore;
 var if_compiling = 0;
+function compile_dict(req_dict) {
+	// console.log('#102: compile_dict');
+	var entries = req_dict.response.split(/[\r\n]+/); // req_dict.response is the text of the dict file
+	
+	var os = db.transaction(["mon_verb"], "readwrite").objectStore("mon_verb");
+
+	for (var i = 0; i < entries.length-1; i++) { // entries.length-1 to strip the blank entry after the last newline
+		var items = entries[i].split("\t");
+		os.add({ id: i, graph: xlit2graph(items[0]), graph_ax: confuse_ax(xlit2graph(items[0])), xlit: items[0], phone: items[1] });
+	}
+	refresh_LB_deconj_lemma();
+	console.log("Dictionary compiled. Reload?");
+	// console.log('#102# compile_dict');
+}
 function send_req_dict(compile_dict) {
 	var req_dict = new XMLHttpRequest();
 	req_dict.onreadystatechange = function() {
 		if (this.readyState == 4 && this.status == 200) {
-			console.log('#101: req_dict.onreadystatechange');
-			console.log("Dictionary downloaded. Compiling...");
+			// console.log('#101: req_dict.onreadystatechange');
+			console.log("Dictionary downloaded. Compiling dictionary; please wait... Retry?");
 			compile_dict(this);
-			console.log('#101# req_dict.onreadystatechange');
+			// console.log('#101# req_dict.onreadystatechange');
 		}
 	};
 	console.log("Downloading dictionary; please wait... Retry?");
 	req_dict.open("GET", "dict.txt", true);
 	req_dict.send();
 }
-function compile_dict(req_dict) {
-	console.log('#102: compile_dict');
-	var entries = req_dict.response.split(/[\r\n]+/); // req_dict.response is the text of the dict file
-	
-	var objectStore = db.transaction(["mon_verb"], "readwrite").objectStore("mon_verb");
-
-	for (var i = 0; i < entries.length-1; i++) { // entries.length-1 to strip the blank entry after the last newline
-		var items = entries[i].split("\t");
-		objectStore.add({ id: i, graph: xlit2graph(items[0]), graph_ax: confuse_ax(xlit2graph(items[0])), xlit: items[0], phone: items[1] });
-	}
-	refresh_LB_deconj_lemma();
-	console.log("Dictionary compiled. Reload?");
-	console.log('#102# compile_dict');
-}
-function open_dict(req_dict) {
+function open_db() {
 	//prefixes of implementation that we want to test
 	window.indexedDB = window.indexedDB || window.mozIndexedDB || window.webkitIndexedDB || window.msIndexedDB;
 	//prefixes of window.IDB objects
@@ -41,36 +40,45 @@ function open_dict(req_dict) {
 	}
 
 	try {
-		var req_db = window.indexedDB.open("db_dict", 1); // version number
+		var req_open_db = window.indexedDB.open("db_dict", 1); // version number
 		console.log('#0: openning of DB requested')
 	} catch(err) {console.error(err)}
-
-	req_db.onupgradeneeded = function(event) {
-		console.log('#1: req_db.onupgradeneeded');
-		db = event.target.result; // or this.result, or req_db.result; // get db
-		objectStore = db.createObjectStore("mon_verb", {keyPath: "id", autoIncrement: true});
-		objectStore.createIndex("by_graph", "graph", {unique: false});
-		objectStore.createIndex("by_graph_ax", "graph_ax", {unique: false});
+		
+	req_open_db.onblocked = function(event) {
+		// If some other tab is loaded with a version lower than requested of the database, then it needs to be closed before we can proceed.
+		alert("Please close all other tabs with this site open! | 请关闭其他由该站点打开的本标签页！");
+	};
+	
+	req_open_db.onupgradeneeded = function(event) { // if the requested version is higher than the local version
+		console.log('#1: req_open_db.onupgradeneeded');
+		db = event.target.result; // or this.result, or req_open_db.result; // get db
+		os = db.createObjectStore("mon_verb", {keyPath: "id", autoIncrement: true});
+		os.createIndex("by_graph", "graph", {unique: false});
+		os.createIndex("by_graph_ax", "graph_ax", {unique: false});
 		if_compiling = 1;
 		send_req_dict(compile_dict);
-		console.log('#1# req_db.onupgradeneeded');
+		console.log('#1# req_open_db.onupgradeneeded');
 	}
 	
-	req_db.onsuccess = function(event) {
-		console.log("#2: req_db.onsuccess");
+	req_open_db.onsuccess = function(event) {
+		console.log("#2: req_open_db.onsuccess");
+		db = event.target.result; // get db
+		db.onversionchange = event => { // new version requested, or deleted, in another tab
+			db.close();
+			alert("A new version of this page is ready. Please reload or close this tab! | 本页面有新版本。请重载或关闭本标签页！");
+		};
 		if (!if_compiling) {
-			db = event.target.result; // get db
 			refresh_LB_deconj_lemma();
 			console.log("Dictionary loaded last time.");
 		}
-		console.log("#2# req_db.onsuccess");
+		console.log("#2# req_open_db.onsuccess");
 	};
 
-	req_db.onerror = function(event) {
-		console.log("#999: ", req_db.error.name);
+	req_open_db.onerror = function(event) { // if the requested version is lower than the local version
+		console.log("#9: ", req_open_db.error.name);
 	};
 }
-open_dict();
+open_db();
 function confuse_ax(s, if_suffix=0) {
 	if(if_suffix) return s.replace(/ᡍ(?=[^ᡃ])/g, "ᡄᡄ");
 	else return s.slice(0, 1)+s.slice(1).replace(/ᡍ(?=[^ᡃ])/g, "ᡄᡄ");
@@ -84,10 +92,9 @@ function lookup_by_graph (graph, if_confuse_teeth=0) { // async request for appe
 	} catch(err) {console.error(err)}
 	var index_name = if_confuse_teeth ? "by_graph_ax" : "by_graph";
 	graph = if_confuse_teeth ? confuse_ax(graph) : graph;
-	// console.log("lookup_by_graph", if_confuse_teeth, graph);
+	// console.log("lookup_by_graph:", "if_confuse_teeth=", if_confuse_teeth, "index_name=", index_name, "graph=", graph);
 	var req_query = tx.objectStore("mon_verb").index(index_name).openCursor(IDBKeyRange.only(graph));
 
-	// console.log("index_name", index_name, "graph", graph);
 	req_query.onsuccess = function() {
 		var cursor = req_query.result;
 		if (cursor) {
@@ -103,5 +110,4 @@ function lookup_by_graph (graph, if_confuse_teeth=0) { // async request for appe
 			// console.log("id", cursor.value.id);
 		}
 	};
-
 }
